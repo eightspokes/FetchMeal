39897056def79c1c32dfe5f4cf8a9bbd2189897c
romanmuni8 <romanmuni8@gmail.com>
2023-05-03 15:26:32 -0400

    Initial Commit

---
+++ FetchMeal/FetchMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchMealApp.swift
+//  FetchMeal
+//
+//  Created by Roman on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
